50ff2feae45399e861d43d2552dc3cf96c12abdd
reyohoho <reyohoho@proton.me>
2025-03-15 16:03:29 +0300

    Add jQuery

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router'; // Import the router
 import VueCookies from 'vue3-cookies';
 import store from './store/store';
+import jQuery from "jquery";
+const $ = jQuery;
+window.$ = $;
 
 createApp(App)  // Initialize the Vue app
   .use(VueCookies)
