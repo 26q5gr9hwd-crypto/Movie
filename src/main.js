f76156b51a12fd1fbfe67549a85167cb3c2fb9e7
reyohoho <reyohoho@proton.me>
2025-03-18 12:20:21 +0300

    Add ya.metrika

--- src/main.js
+++ src/main.js
@@ -4,11 +4,19 @@ import router from './router'; // Import the router
 import VueCookies from 'vue3-cookies';
 import store from './store/store';
 import jQuery from "jquery";
+import { initYandexMetrika } from 'yandex-metrika-vue3';
 const $ = jQuery;
 window.$ = $;
 
-createApp(App)  // Initialize the Vue app
+const app = createApp(App)
+
+app
   .use(VueCookies)
-  .use(router)  // Use the router
-  .use(store) 
-  .mount('#app')  // Mount the app to the DOM
+  .use(router)
+  .use(store)
+  .use(initYandexMetrika, {
+    id: 94822173,
+    router: router,
+    env: process.env.NODE_ENV
+  })
+  .mount('#app')
