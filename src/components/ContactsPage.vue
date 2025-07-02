7675f35581e319c4555f20000f43ba539ce0b794
reyohoho <reyohoho@proton.me>
2025-07-02 07:31:45 +0300

    Contacts update

--- src/components/ContactsPage.vue
+++ src/components/ContactsPage.vue
@@ -2,23 +2,13 @@
   <div class="contact-text">
     <p>
       Если Вы владелец авторских прав, просим Вас учесть, что все плееры на сайте из сторонних API
-      источников. Пишите напрямую по контактным данным этих видео-хостингов, для соблюдения DMCA. В
-      99% случаев они удаляют спорный контент в течение 24 часов. Плееры недоступные на
-      первоисточнике априори не будут отображаться на ReYohoho. Или напишите на почту
-      <a href="mailto:reyohoho@proton.me">reyohoho@proton.me</a> или в Телеграм
-      <a href="https://t.me/reyohoho_sup" target="_blank" rel="noopener noreferrer"
-        >https://t.me/reyohoho_sup</a
-      >
+      источников. Напишите на почту или в Телеграм с подтверждением авторских прав для удаления
+      контента с сайта.
     </p>
     <p>
       If you are a copyright owner, please note that all players on the site are from third-party
-      API sources. Please write directly to the contact details of these video hosting sites to
-      comply with the DMCA. In 99% of cases, they remove controversial content within 24 hours.
-      Players that are not available on the original source will a priori not be displayed on
-      ReYohoho. Or write to <a href="mailto:reyohoho@proton.me">reyohoho@proton.me</a> or Telegram:
-      <a href="https://t.me/reyohoho_sup" target="_blank" rel="noopener noreferrer"
-        >https://t.me/reyohoho_sup</a
-      >
+      API sources. Please write to email or Telegram with confirmation of copyright for the removal
+      of content from the site.
     </p>
     <p>
       <a href="mailto:reyohoho@proton.me">reyohoho@proton.me</a> |
