d21bbc9684db53848f6fed7d9fb5af4127f9b4f0
olegsvs <olegsvs@users.noreply.github.com>
2025-07-20 21:00:35 +0000

    deploy: 26c700b8445d7fb2f5cd3b6564bebf070e956a23

--- assets/trailer-legacy-DrJarXyS.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-Dy8d9Hy6.js"],(function(e,r){"use strict";var t;return{setters:[e=>{t=e.au}],execute:function(){e("u",t("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0}))}}}));
