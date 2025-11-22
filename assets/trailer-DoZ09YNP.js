b65e4d1337209cbc2e3ae2abc9acc38eaaccbdbe
reyohoho <reyohoho@users.noreply.github.com>
2025-11-22 10:06:26 +0000

    deploy: d0af623b6ba5ff4b3608241759a969c793379c21

--- assets/trailer-DoZ09YNP.js
+++
@@ -1,1 +0,0 @@
-import{aw as e}from"./index-CCKmU0_p.js";const t=e("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0});export{t as u};
