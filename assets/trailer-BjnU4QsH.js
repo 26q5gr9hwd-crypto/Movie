53aa61645b0d6e942d969ab582a9112c6750f37c
reyohoho <reyohoho@users.noreply.github.com>
2025-12-04 04:30:10 +0000

    deploy: b127a5a860010997196c132a6170b58c86ae1f17

--- assets/trailer-BjnU4QsH.js
+++
@@ -1,1 +0,0 @@
-import{aw as e}from"./index-D0riJ2ft.js";const t=e("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0});export{t as u};
