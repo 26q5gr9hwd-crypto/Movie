d6c6ba65ee4f2b4292696302e29630c4a2abc962
Dan <26q5gr9hwd@privaterelay.appleid.com>
2026-02-02 12:18:17 +0300

    feat: add toast notification composable

---
+++ src/composables/useToast.js
@@ -0,0 +1,17 @@
+import { ref } from 'vue'
+
+const toasts = ref([])
+let toastId = 0
+
+export function useToast() {
+  const showToast = (message, duration = 3000) => {
+    const id = toastId++
+    toasts.value.push({ id, message })
+
+    setTimeout(() => {
+      toasts.value = toasts.value.filter(t => t.id !== id)
+    }, duration)
+  }
+
+  return { toasts, showToast }
+}
