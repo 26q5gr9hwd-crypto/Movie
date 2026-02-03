12d192b32ab8848054e776c5e69839a8a04f4e68
Dan <26q5gr9hwd@privaterelay.appleid.com>
2026-02-03 14:19:17 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { useApiStore } from '@/store/api'
 
 const firebaseConfig = {
