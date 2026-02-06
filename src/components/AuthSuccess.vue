9fe03f4cb7d520bb3126ed1f8c8ca164839425c7
Dan <26q5gr9hwd@privaterelay.appleid.com>
2026-02-06 14:35:20 +0300

    fix: add missing AuthSuccess.vue placeholder to fix build

---
+++ src/components/AuthSuccess.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="auth-success-page">
+    <h2>Вход выполнен отлично!</h2>
+    <p>Вы будете перенаправлены на главную страницу...</p>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'AuthSuccess',
+  setup() {
+    const router = useRouter()
+    onMounted(() => {
+      setTimeout(() => {
+        router.push('/')
+      }, 2000)
+    })
+  }
+}
+</script>
