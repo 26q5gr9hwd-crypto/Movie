293f618092a8070bf88356c38a03940fea004188
Dan <26q5gr9hwd@privaterelay.appleid.com>
2026-02-06 14:34:14 +0300

    fix: add missing UserLists.vue placeholder to fix build

---
+++ src/components/UserLists.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="user-lists-page">
+    <h2>Списки</h2>
+    <p>Функция списков находится в разработке.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserLists'
+}
+</script>
