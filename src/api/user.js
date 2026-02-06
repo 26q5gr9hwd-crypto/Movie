076b3db9c2efb3df726a8e9ed7d1f2fd50740328
Dan <26q5gr9hwd@privaterelay.appleid.com>
2026-02-06 17:07:14 +0300

    fix: add missing addToList export to user.js

--- src/api/user.js
+++ src/api/user.js
@@ -6,6 +6,12 @@ export const getMyLists = async (listType) => {
   return data
 }
 
+export const addToList = async (kpId, listType) => {
+  const api = await getApi()
+  const { data } = await api.post('/user/lists/' + listType + '/' + kpId)
+  return data
+}
+
 export const delFromList = async (kpId, listType) => {
   const api = await getApi()
   const { data } = await api.delete('/user/lists/' + listType + '/' + kpId)
